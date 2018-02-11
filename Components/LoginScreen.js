a6f24753ee71c391080949e9e4ee7ffcf3190b94
RaviTeja <raviteja@homes-MacBook-Air.local>
2018-02-11 14:12:08 +0530

    Login Component

---
+++ Components/LoginScreen.js
@@ -0,0 +1,23 @@
+import {Component} from 'react'
+import {View} from 'react-native'
+export default class LoginScreen extends Component{
+
+constructor(props){
+    super(props)
+}
+
+render(){
+    return(
+        <View style={LoginViewStyle.container}>
+         </View>
+    )
+}
+}
+
+const LoginViewStyle = ReactNative.createStyle({
+
+    container : {
+
+    }
+
+});
