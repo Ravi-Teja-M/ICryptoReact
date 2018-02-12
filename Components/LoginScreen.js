1cf874c328f19427e55c82c7de17c145c6ecb5a0
raviteja <raviteja.maddali@centrica.com>
2018-02-12 14:34:56 +0530

    Navigation Component + login Screen

--- Components/LoginScreen.js
+++ Components/LoginScreen.js
@@ -1,27 +1,184 @@
 import React,{Component} from 'react'
 import 
 {
-    View,
-    StyleSheet
+    View, Text, TextInput,Button, StyleSheet ,ActivityIndicator ,NavigatorIOS
 } from 'react-native'
 export default class LoginScreen extends Component{
 
 constructor(props){
     super(props)
+    this.initState();
+}
+
+initState(){
+    this.state ={
+        timer : new Date().toLocaleString(),
+        userName: "ravi-teja-m",
+        password: "Ironman!234",
+        isLoading :false,
+        isErrorEnabled : false,
+        isLoggedInSuccessful: false,
+        loggedUserName: ""
+    }
+
 }
 
 render(){
-    return(
-        <View style={LoginViewStyle.container}>
-         </View>
-    )
+      
+    const LoginContainer = this.LoginScreen()
+    return  ( <View>
+                {LoginContainer}
+             </View>
+     )
 }
-}
 
-const LoginViewStyle = StyleSheet.create({
+LoginScreen (){
+    var errorInput = <View/>
+    var userProfileGreeting = <View/>
 
-    container : {
-
+    if(this.state.isErrorEnabled){
+        errorInput= <Text style={stylingView.errormesg} > invalid credentials, kindly re-enter.</Text> ;
     }
 
+    if(this.state.isLoggedInSuccessful){
+        userProfileGreeting = <Text style={stylingView.greetings} > Welcome {this.state.loggedUserName}..! </Text> ;
+    }
+
+    return (
+        <View style={{flexDirection: 'column',width: '100%',height:'100%',alignContent:"center",backgroundColor : 'grey'}}>
+        <View style={{marginTop:'50%'}}>
+        <View style={{flexDirection: 'row',margin:20 }}>
+            <Text style={stylingView.textField} >Email: </Text>
+            <TextInput ref={(element)=>{ this.userName = element; }} 
+                       id="userName"  style={stylingView.textInput} 
+                        onChangeText={this.onUserTextChange.bind(this)} 
+                        value={this.state.userName} >
+            </TextInput>
+        </View>
+
+        <View style={{flexDirection: 'row',margin:20}}>
+            <Text style={rs.textField}>Password: </Text>
+            <TextInput ref={(element)=>{ this.password = element;}}
+                       onChangeText={this.onPasswordTextChange.bind(this)} 
+                       style={stylingView.textInput} 
+                       value={this.state.password} >
+            </TextInput>
+
+        </View>
+        <Button style={stylingView.submitStyle} title="Submit" onPress={this.onSubmitClicked.bind(this)}> </Button>
+         <ActivityIndicator progressStyle={stylingView.progressS} animating={this.state.isLoading} ></ActivityIndicator>
+            { errorInput }
+
+            {userProfileGreeting}
+            </View>
+        </View>
+    )
+}
+
+onUserTextChange(eventObject){
+    console.log(eventObject);
+    this.setState(function(previous,props){
+        return {userName: eventObject};
+    });
+}
+
+onPasswordTextChange(eventObject){
+    console.log(eventObject);
+    this.setState((previous,props)=>{
+        return {password: eventObject};
+    });
+}
+
+onSubmitClicked(event){  
+    if(this.state.userName.trim().length ==0){        
+       this.setState( {isErrorEnabled: true}
+       );
+        return;
+    }
+    if(this.state.password.trim().length == 0) {
+       this.setState( {isErrorEnabled: true}
+       );
+       return;
+    }
+    this.setState((previous,props)=>{
+       return {isLoading: true};
+   });
+
+   this.setState( {isErrorEnabled: false}
+   );
+
+   console.log('state' + this.state.isLoading);
+ }
+//eof
+}
+
+const stylingView = StyleSheet.create({
+    greetings:{
+        color: 'white',
+        fontSize: 20,
+        width: '100%',
+        textAlign: 'center'
+    },
+    errormesg:{
+        color: 'red',
+        fontSize: 20,
+        width: '100%',
+        textAlign: 'center'
+    } ,
+    progressS:{
+        backgroundColor : 'red',
+    } ,
+    block:{
+        backgroundColor:'yellow',
+        marginTop:2,
+        marginLeft:2,
+        flex: 1
+    },
+    submitStyle:{
+        width: '100%',
+        height: 80,
+        backgroundColor:'red',
+        marginTop:2,
+        marginLeft:2,
+        flex: 1,        
+        color:'white'
+    },
+    textField :{
+        color: '#FFFFFF',
+        backgroundColor : '#82B1FF',
+        flex:0.3,
+        marginTop:20,
+        padding:10
+     },
+    textInput :{
+        color: '#000000',
+        backgroundColor : '#ffffff',
+        flex:0.7,
+        marginTop:20,
+        padding:10
+    },
+    loginLabel :{
+        color: '#FFFFFF',
+         width:"100%",
+        marginTop:30,
+        padding:10,
+        textAlign:"left",
+        fontSize: 18,
+        paddingLeft: 25
+    },
+    timeTicker :{
+        color: '#ffffff',
+         width:"100%",
+        marginTop:30,
+        padding:10,
+        textAlign:"left",
+        fontSize: 15
+     },
+    progress :{
+    }
 });
+
+const rs = Object.create(stylingView);
+rs.prototype = {
+
+}
--- Components/SplashScreen.js
+++ Components/SplashScreen.js
@@ -2,8 +2,14 @@ import React,{Component} from 'react'
 import 
 {
     View,
-    StyleSheet
+    StyleSheet,
+    Image,
+    Text
 } from 'react-native'
+import applogo from '../assets/images/icon_.png'
+import reactlogo from '../assets/images/react_icon.png'
+
+
 export default class SplashScreen extends Component{
 
 constructor(props){
@@ -11,17 +17,67 @@ constructor(props){
 }
 
 render(){
+    const appTitle = `( Ravi ) => { "iCrypto" }
+        `
+ 
     return(
-        <View style={SplashScreen.container}>
+        <View style={SplashScreenStyle.container}>
+        <Image style={SplashScreenStyle.logoStyle} source={applogo} ></Image>
+
+        <Text style={SplashScreenStyle.appTitle}> {appTitle} </Text>
+        <Image style={SplashScreenStyle.reactlogoStyle} source={reactlogo} ></Image>
+
+ 
          </View>
     )
 }
+
+componentDidMount(){
+   var intervalInst= setInterval(()=>{
+     clearInterval(intervalInst);
+
+     //Navigate to Login Screen or check for Existing credentials
+
+    },  1500);
 }
 
-const SplashScreen = StyleSheet.create({
+hasLoginCredentials(){
 
+    return false
+}
+
+navigateToLoginScreen(){
+
+}
+
+navigateToDashBoardScreen(){
+
+}
+
+//EOF
+}
+const SplashScreenStyle = StyleSheet.create({
+    appTitle:{
+        alignSelf: 'center',
+        marginTop: 50,
+        fontSize: 20,
+        fontStyle: 'normal',
+        fontWeight: 'bold'
+    },
+    logoStyle:{
+         alignSelf: 'center',
+         marginTop: '25%',
+    },
+    reactlogoStyle:{
+        marginTop: 20,
+
+        alignSelf: 'center',
+        },
     container : {
-        
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'white',
+   
     }
 
 });
